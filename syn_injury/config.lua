e01cc0b76324fcc6853b3895734e94ed75696e87
entarukun <76920369+entarukun@users.noreply.github.com>
2021-01-14 02:33:40 -0600

    Add files via upload

---
+++ syn_injury/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.UseBleed = true -- adds bleed damage at 140 health
+
+Config.UseWeak = true    -- lean over when very hurt 
+
+Config.UseFaded = true -- screen effect and sound echo 
+
+Config.UseWobble = true     -- screen drunk wobble 
