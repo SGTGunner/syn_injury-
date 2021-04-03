fe01e1d0b0063bf1224631291869ad957070c8a3
entarukun <76920369+entarukun@users.noreply.github.com>
2021-04-02 19:39:39 -0500

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'injury'
+
+version '1.2.0'
+
+
+client_scripts {
+	'main.lua',
+   'config.lua',
+   'functions.lua'
+  
+  }
+  
+  server_scripts {
+	'config.lua'
+  
+  }
